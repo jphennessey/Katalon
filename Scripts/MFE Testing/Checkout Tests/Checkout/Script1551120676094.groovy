11c9a64f6671f338ac5cba9ff09961e992f25626
Joe Hennessey <4jp.hennessey@gmail.com>
2019-02-28 11:32:13 -0500

    swapped delays with wait for element; updated .gitignore

--- Scripts/MFE Testing/Checkout Tests/Checkout/Script1551120676094.groovy
+++ Scripts/MFE Testing/Checkout Tests/Checkout/Script1551120676094.groovy
@@ -52,19 +52,15 @@ WebUI.click(findTestObject('MFE Testing Pages/Page_PDP/button_viewCart'))
 
 WebUI.click(findTestObject('MFE Testing Pages/Cart/button_SecureCheckout'))
 
-WebUI.delay(1)
-
 WebUI.click(findTestObject('MFE Testing Pages/Page_Checkout/button_SaveAndContinue'))
 
 WebUI.delay(1)
 
 WebUI.click(findTestObject('MFE Testing Pages/Page_Checkout/button_SaveAndContinue'))
 
-WebUI.delay(1)
+WebUI.waitForElementClickable(findTestObject('MFE Testing Pages/Page_Checkout/3-ReviewAndSubmit/button_PlaceOrder'), 1)
 
 WebUI.click(findTestObject('MFE Testing Pages/Page_Checkout/3-ReviewAndSubmit/label_IamAtLeast21yearsOldCheckbox'))
-
-WebUI.delay(1)
 
 WebUI.click(findTestObject('MFE Testing Pages/Page_Checkout/3-ReviewAndSubmit/button_PlaceOrder'))
 
